72af040bbd63218f78ed315002100ca6fdf16a00
RajPathare <rajgpathare@gmail.com>
2020-10-05 23:38:59 +0530

    nav scratch

--- ReactCourse/widgets/src/App.js
+++ ReactCourse/widgets/src/App.js
@@ -3,6 +3,8 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
+import Header from './components/Header';
 
 const items = [
     {
@@ -40,10 +42,23 @@ const App = () => {
 
     return (
         <div>
+            <Header />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown label="Select a color!" options={options} selected={selected} onSelectedChange={setSelected} />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
             {/* <Accordion items={items}/> */}
             {/* <Search /> */}
             {/* <Dropdown label="Select a color!" options={options} selected={selected} onSelectedChange={setSelected} /> */}
-            <Translate />
+            {/* <Translate /> */}
         </div>
     );
 }

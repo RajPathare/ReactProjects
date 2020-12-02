466132d67523232fc2e9cb85391f12f510f54dc1
RajPathare <rajgpathare@gmail.com>
2020-12-02 14:42:31 +0530

    completed stream show

--- ReactCourse/TwitchCourse/streams/client/src/components/App.js
+++ ReactCourse/TwitchCourse/streams/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Router, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 
 import StreamCreate from './streams/StreamCreate';
 import StreamEdit from './streams/StreamEdit';
@@ -21,11 +21,14 @@ const App = () => {
             <div>
             {/* Header must be inside BrowserRouter since we are using Link tag in Header and Link cannot be used outside a Router (BrowserRouter) */}
                 <Header/> 
-                <Route path="/" exact component={StreamList} />
-                <Route path="/streams/new" exact component={StreamCreate} />
-                <Route path="/streams/edit/:id" exact component={StreamEdit} />
-                <Route path="/streams/delete/:id" exact component={StreamDelete} />
-                <Route path="/streams/show" exact component={StreamShow} />
+                {/* switch is used to avoid mixing of multiple route components (sometimes the components are shown together if we try to access a particular route like /stream/new and /streams/:id, react thinks that new is the variable :id and shows these components together) */}
+                <Switch> 
+                    <Route path="/" exact component={StreamList} />
+                    <Route path="/streams/new" exact component={StreamCreate} />
+                    <Route path="/streams/edit/:id" exact component={StreamEdit} />
+                    <Route path="/streams/delete/:id" exact component={StreamDelete} />
+                    <Route path="/streams/:id" exact component={StreamShow} />
+                </Switch>
             </div>
             </Router>
         </div>
